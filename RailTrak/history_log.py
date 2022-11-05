1ae573534445d1c6be0b9e34a6e2432bed64d2be
Dan Bouzolin <6091120+moonpockets@users.noreply.github.com>
2022-11-05 16:16:34 -0500

    login feature

---
+++ RailTrak/history_log.py
@@ -0,0 +1,15 @@
+﻿from pywebio.input import *
+from pywebio.output import put_text
+from pywebio import start_server
+
+def userValidation(username, password):
+    pass
+
+def show_histlog():
+    login = input_group("RailTrac Login", [
+        input('Username', name='username'),
+        input('Password', name='pass', type=PASSWORD)])
+
+
+
+start_server(show_histlog, port=80)
