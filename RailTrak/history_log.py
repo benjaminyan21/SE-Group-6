8beec8a022773c498d148c1e3ddaa56ac25c5364
Dan Bouzolin <6091120+moonpockets@users.noreply.github.com>
2022-11-05 16:49:10 -0500

    added login feature

--- RailTrak/history_log.py
+++ RailTrak/history_log.py
@@ -3,13 +3,33 @@ from pywebio.output import put_text
 from pywebio import start_server
 
 def userValidation(username, password):
-    pass
+    with open('users.txt', 'r') as filestream:
+        for line in filestream:
+            currentLine = line.split(",")
+            if (username == currentLine[0] and password == currentLine[1].rstrip()):
+                return None
+            else: 
+                print(username)
+                print(currentLine[0])
+                print(password)
+                print(currentLine[1].rstrip())
+        return 'Username or password is incorrect'
 
-def show_histlog():
+def showLogin():
     login = input_group("RailTrac Login", [
         input('Username', name='username'),
         input('Password', name='pass', type=PASSWORD)])
 
+    return login['username'], login['pass']
 
+def show_histlog():
+    username, password = showLogin()
+    msg = userValidation(username, password)
 
-start_server(show_histlog, port=80)
+    while (msg is not None):
+        put_text(msg)
+        showLogin()
+
+    put_text('ohyeah baby swag')
+
+start_server(show_histlog, port=80, debug=True)
