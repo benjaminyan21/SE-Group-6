ede92b00099673b080c1179d198ae4c76357cb5d
Marcey <MarcelaIpantoja@gmail.com>
2022-11-06 01:27:43 -0600

    Messing with some more things

--- RailTrak/RailTrak/RailTrak.py
+++ RailTrak/RailTrak/RailTrak.py
@@ -4,22 +4,49 @@ from pywebio import start_server
 
 def TrackGUI():
 
+    **This is for the Route Information enetered by the User**
     data= input_group("Route Information", [
-        input('To: ', StartingPoint = 'location', validate=location_check),
-        input('From', Destination = 'location', validate=location_check)
+        input('To: ', StartingPoint = 'StartingPoint', validate=location_check),
+        input('From', Destination = 'Destination', validate=location_check)
         ])
 
-    output.put_text("Here is the route from", StartingPoint, " to", destination)
+    **Some text informing the user that their route is being displayed**
+    put_text("Here is the route from", data['StartingPoint'], " to ", data['Destination'])
 
-
-    
+    **This is not configured yet, but will display the best route for the two entered locations**
     output.put_image(src, format=None, title='', width=None, height=None, scope=None, position=- 1)
 
+    **This is a button that will be on the screen, it is going to display the route information when clicked**
+    put_button("Route Information", onclick=lambda: toast(RouteInformation), color='success', outline=True)
 
+    put_button("Toggle Map", onclick=lambda: toast("Will toggle change in map type"), color='success', outline=True)
+
+    put_button("Zoom in", onclick=lambda: toast("Will zoom in on map"), color='success', outline=True)
+
+    put_button("Zoom out", onclick=lambda: toast("Will zoom out on map"), color='success', outline=True)
+
+
+
+**This is for validating the entered locations as we are having constrictions on what cities you can user, ect.**
 def location_check(data):
 
-    output.put_text("I'm sorry, but the location you entered as not valid")
+    if len(data['StartingPoint']) != 'ValidLocation':
+        return('StartingPoint', ' is not a Valid Location')
+
+
+    if len(data['Destination']) != 'ValidLocation':
+        return('Destination', ' is not a Valid Location')
+
+def RouteInformation(data):
+
+    **going to need the database to be configured for this, and for most of the buttons i think**
+    popup('Route Information', "Information for a route from", 'StartingPoint', "to", 'Destination')
+
+
+
+
     
 
 
+**this is for the server**
 start_server(TrackGUI, port=8080, debug=True)
