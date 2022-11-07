e8e2df1a8bfa047218b51242f088c8526094fdf4
Dan Bouzolin <6091120+moonpockets@users.noreply.github.com>
2022-11-06 21:08:52 -0600

    added filtering capability for history log

--- RailTrak/history_log.py
+++ RailTrak/history_log.py
@@ -3,6 +3,7 @@ from pywebio.output import *
 from pywebio import start_server
 from UserDBM import UserDBM
 
+# Define the login function to show and receive input from user
 def showLogin():
     login = input_group("RailTrac Login", [
         input('Username', name='username'),
@@ -10,18 +11,42 @@ def showLogin():
 
     return login['username'], login['pass']
 
+# The main webpage for the history log.
+# This page shows the recent searches a user has made, based on
+# departure location, departure time, arrival location, and arrival time
 def showHistoryLog():
-    put_button("Home", onclick=lambda: showMenu(), color='success', outline=True)
+    clear()
+    put_row([put_button("Home", onclick=lambda: showMenu(), color='success', outline=True),
+             put_button("Logout", onclick=lambda: loginPage(), color='success', outline=True)])
     put_markdown(r""" # RailTrac History Log
-    Your recent searches:
     """)
+    put_text('Welcome, ' + username + '!')
+    put_text('Your recent searches:')
     userDB = UserDBM('userDB.txt')
-    userHistory = userDB.readUserHistory(username)
-    put_text('Departure: ' + userHistory.departureLocation +
-             ' (' + userHistory.departureTime + ')')
-    put_text('Arrival: ' + userHistory.arrivalLocation +
-             ' (' + userHistory.arrivalTime + ')')
+    with use_scope() as newscope:
+        userHistory = userDB.readUserHistory(username)
+        put_text('Departure: ' + userHistory.departureLocation +
+                 ' (' + userHistory.departureTime + ')')
+        put_text('Arrival: ' + userHistory.arrivalLocation +
+                 ' (' + userHistory.arrivalTime + ')')
 
+    # Allow the user to filter through their searches by certain criteria
+    filterby = select(['Filter Departure Location'], ['Dallas TX', 'Austin TX', 'Houston TX'])
+    while (True):
+        if (userHistory.departureLocation == filterby):
+            clear(newscope)
+            with use_scope() as newscope:
+                put_text('Departure: ' + userHistory.departureLocation +
+                     ' (' + userHistory.departureTime + ')')
+                put_text('Arrival: ' + userHistory.arrivalLocation +
+                     ' (' + userHistory.arrivalTime + ')')
+                filterby = select(['Filter Departure Location'], ['Dallas TX', 'Austin TX', 'Houston TX'])
+        else:
+            clear(newscope)
+            filterby = select(['Filter Departure Location'], ['Dallas TX', 'Austin TX', 'Houston TX'])
+
+
+# This function shows the main RailTrac menu from which the user can navigate to the history log
 def showMenu():
     clear()
     put_markdown(r""" # Welcome to RailTrac!
@@ -29,7 +54,9 @@ def showMenu():
     """)
     put_link('History Log',app='showHistoryLog')
 
+# This function gets the login from the user and validates it against the database
 def loginPage():
+    clear()
     global username
     username, password = showLogin()
     userDB = UserDBM('userDB.txt')
@@ -43,9 +70,11 @@ def loginPage():
 
     showMenu()
 
+# Index / homepage of the RailTrac website
 def index():
     put_markdown(r""" # RailTrac Homepage
     """)
     put_link('Returning User Login', app='loginPage')
 
+# Start the RailTrac application
 start_server([index, loginPage, showHistoryLog], port=80, debug=True, remote_access=True)
