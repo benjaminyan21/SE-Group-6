9629ccde98669407ebc896b0a36747300280fa81
Dan Bouzolin <6091120+moonpockets@users.noreply.github.com>
2022-11-29 22:39:18 -0600

    multiple entries for user history

--- RailTrak/RailTrak/history_log.py
+++ RailTrak/RailTrak/history_log.py
@@ -31,27 +31,62 @@ def showHistoryLog():
         put_text('---')
 
     # Allow the user to filter through their searches by certain criteria
-    #filterby = 
-    put_select('in1', ['Dallas TX', 'Austin TX', 'Houston TX'], label='Filter Departure Location')
-    put_buttons(['Submit'], lambda _: put_historyLog())
-    put_buttons(['Test User History'], lambda _: userDB.writeUserHistory('Austin TX', 'Dallas TX', 0.5))
-    while (get_scope() == newscope):
-        put_buttons(['Submit'], lambda _: put_historyLog())
-        if (userHistory.departureLocation == pin.in1):
-            clear(newscope)
-            with use_scope() as newscope:
-                put_text('Departure: ' + userHistory.departureLocation)
-                put_text('Arrival: ' + userHistory.arrivalLocation)
-                minutes = float(userHistory.eta)*60
-                put_text('ETA: ' + str(minutes) + " minutes")
-                #filterby = 
-                put_select('in1', ['Dallas TX', 'Austin TX', 'Houston TX'], label='Filter Departure Location')
-        else:
-            clear(newscope)
-            #filterby = 
-            put_select('in1', ['Dallas TX', 'Austin TX', 'Houston TX'], label='Filter Departure Location')
-            while True:
-                pin_wait_change('in1')
+    put_select('in1', ['All', 'New York City', 'Chicago', 'Boston', 'Washington DC'], label='Filter Departure Location')
+    put_buttons(['Submit'], lambda _: clear(newscope) and put_historyLog())
+    #put_buttons(['Test User History'], lambda _: userDB.writeUserHistory('Austin TX', 'Dallas TX', 0.5))
+    cont = True
+    while (cont):
+        pin_update('in1')
+        clear(newscope)
+        with use_scope() as writescope:
+            if (pin.in1 == "All"):
+                count = 0
+                while (count < userHistory.numSearches):
+                    put_text('---')
+                    put_text('Departure: ' + userHistory.departureLocation[count])
+                    put_text('Arrival: ' + userHistory.arrivalLocation[count])
+                    minutes = float(userHistory.eta[count])*60
+                    put_text('ETA: ' + str(minutes) + " minutes")
+                    count += 1
+                put_text('---')
+            else:
+                for i, x in enumerate(userHistory.departureLocation):
+                    if (x == pin.in1):
+                        put_text('Departure: ' + userHistory.departureLocation[i])
+                        put_text('Arrival: ' + userHistory.arrivalLocation[i])
+                        minutes = float(userHistory.eta[i])*60
+                        put_text('ETA: ' + str(minutes) + " minutes")
+            pin_wait_change('in1')
+            clear(writescope)
+
+        #cont = False
+                    #put_select('in1', ['New York City', 'Chicago', 'Boston', 'Washington DC'], label='Filter Departure Location')
+            #else:
+                #pass
+                #clear(newscope)
+                #put_select('in1', ['New York City', 'Chicago', 'Boston', 'Washington DC'], label='Filter Departure Location')
+                #while True:
+                #pin_wait_change('in1')
 
 def put_historyLog():
-    put_text(pin.in1)
+    f = open("currentUser.txt", "r")
+    username = f.read()
+    userDB = UserDBM('userDB.txt')
+    userHistory = userDB.readUserHistory(username)
+
+    with use_scope() as newscope:
+        cont = True
+        while (cont):
+            put_error("A")
+            pin_wait_change('in1')
+            pin_update('in1')
+            #put_buttons(['Submit'], lambda _: put_historyLog())
+            for i, x in enumerate(userHistory.departureLocation):
+                put_error("AA")
+                put_error(userHistory.departureLocation[i])
+                if (x == pin.in1):
+                    put_text('Departure: ' + userHistory.departureLocation[i])
+                    put_text('Arrival: ' + userHistory.arrivalLocation[i])
+                    minutes = float(userHistory.eta[i])*60
+                    put_text('ETA: ' + str(minutes) + " minutes")
+            cont = False
